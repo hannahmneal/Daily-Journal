dff87910c7eb214269a869f8b9fd42569593838b
Hannah Neal <hannahmneal@gmail.com>
2018-12-05 00:10:25 -0600

    added journal.js and format for HTML DOM setup. Also changed options on mood drop-down menu in HTML to emojis (UTF-8). Feeling: nerdy-cool.

---
+++ src/scripts/journal.js
@@ -0,0 +1,15 @@
+// console.log("journal.js test");          // Test. journal.js is linked to index.html.
+
+const entry = [
+    {
+        date: "",
+        concepts: "",
+        entry: "",
+        mood: ""
+    }
+]
+
+const createEntry = (entry) => {
+    //Create your own HTML structure for a journal entry
+    return `<h1 class = "${style}">${title}</h1>`
+}
